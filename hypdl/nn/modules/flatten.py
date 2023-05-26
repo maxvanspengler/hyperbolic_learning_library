60d87dd55d7b027da70824f36f973c5edbbcf0be
philippmwirth <philipp.m.wirth@gmail.com>
2023-05-26 18:44:18 +0200

    Add HFlatten module

---
+++ hypdl/nn/modules/flatten.py
@@ -0,0 +1,23 @@
+from hypdl.tensors import ManifoldTensor
+from torch.nn import Module, functional
+
+
+class HFlatten(Module):
+    """Flattens a contiguous range of dims into a tensor.
+
+    Attributes:
+        start_dim:
+            First dimension to flatten (default = 1).
+        end_dim:
+            Last dimension to flatten (default = -1).
+
+    """
+
+    def __init__(self, start_dim: int = 1, end_dim: int = -1):
+        super(HFlatten, self).__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, x: ManifoldTensor) -> ManifoldTensor:
+        """Flattens the manifold input tensor."""
+        return x.flatten(start_dim=self.start_dim, end_dim=self.end_dim)
---
+++ tests/nn/test_flatten.py
@@ -0,0 +1,49 @@
+import pytest
+from hypdl.nn import HFlatten
+from hypdl.tensors import ManifoldTensor
+from hypdl.manifolds import PoincareBall
+
+
+def test_hflatten():
+    manifold_tensor = ManifoldTensor(
+        data=[
+            [
+                [1.0, 2.0],
+                [3.0, 4.0],
+            ],
+            [
+                [5.0, 6.0],
+                [7.0, 8.0],
+            ],
+        ],
+        manifold=PoincareBall(),
+        man_dim=1,
+        requires_grad=True,
+    )
+
+    hflatten = HFlatten(start_dim=1, end_dim=-1)
+    flattened = hflatten(manifold_tensor)
+    assert flattened.shape == (2, 4)
+
+
+def test_hflatten__raises_value_error():
+    manifold_tensor = ManifoldTensor(
+        data=[
+            [
+                [1.0, 2.0],
+                [3.0, 4.0],
+            ],
+            [
+                [5.0, 6.0],
+                [7.0, 8.0],
+            ],
+        ],
+        manifold=PoincareBall(),
+        man_dim=1,
+        requires_grad=True,
+    )
+
+    hflatten = HFlatten(start_dim=0, end_dim=-1)
+    with pytest.raises(ValueError) as e:
+        flattened = hflatten(manifold_tensor)
+        assert "Can't flatten the manifold dimension 1 of manifold tensor!" in str(e)
