1579279006b79aa88b8a16b37a7c3562ed6b05f2
philippmwirth <philipp.m.wirth@gmail.com>
2023-05-26 18:31:58 +0200

    Fix hardcoded man_dim

--- hypdl/manifolds/poincare_ball/manifold.py
+++ hypdl/manifolds/poincare_ball/manifold.py
@@ -261,5 +261,7 @@ class PoincareBall(Manifold):
             stride=stride,
         )
         new_tensor = new_tensor.transpose(1, 2)
-        new_tensor = TangentTensor(data=new_tensor, manifold_points=None, manifold=self, man_dim=2)
+        new_tensor = TangentTensor(
+            data=new_tensor, manifold_points=None, manifold=self, man_dim=input.man_dim
+        )
         return self.expmap(new_tensor)
