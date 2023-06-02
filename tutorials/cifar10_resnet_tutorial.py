3cb53bffa9cbdc618962b1f8d1dee9fc489e6653
maxvanspengler <max.van.spengler@hotmail.com>
2023-06-02 16:18:43 +0200

    Added rest of Poincare ResNet tutorial

--- tutorials/cifar10_resnet_tutorial.py
+++ tutorials/cifar10_resnet_tutorial.py
@@ -1,3 +1,66 @@
+########################################################################
+# 1. Define a hyperbolic manifold
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# We use the Poincaré ball model for the purposes of this tutorial.
+# This tutorial assumes the availability of a GPU. While training a
+# hyperbolic ResNet without GPUs is possible, we strongly recommend
+# using a GPU for this task.
+
+from hll.manifolds import Curvature, PoincareBall
+
+# Making the curvature a learnable parameter is usually suboptimal but can
+# make training smoother. An initial curvature of 0.1 has also been shown
+# to help during training.
+manifold = PoincareBall(c=Curvature(value=0.1, requires_grad=True))
+
+
+########################################################################
+# 2. Load and normalize CIFAR10
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+########################################################################
+# .. note::
+#     If running on Windows and you get a BrokenPipeError, try setting
+#     the num_worker of torch.utils.data.DataLoader() to 0.
+
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+
+batch_size = 128
+
+trainset = torchvision.datasets.CIFAR10(
+    root="./data", train=True, download=True, transform=transform
+)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=batch_size, shuffle=True, num_workers=2
+)
+
+testset = torchvision.datasets.CIFAR10(
+    root="./data", train=False, download=True, transform=transform
+)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=batch_size, shuffle=False, num_workers=2
+)
+
+
+########################################################################
+# 3. Define a hyperbolic ResNet
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# This implementation is based on the Poincare ResNet paper, which can
+# be found at https://arxiv.org/abs/2303.14027 and which, in turn, is 
+# based on the original Euclidean implementation described in the paper
+# Deep Residual Learning for Image Recognition by He et al. from 2015: 
+# https://arxiv.org/abs/1512.03385.
+
+
 from typing import Optional
 
 from torch import nn
@@ -16,6 +79,7 @@ class PoincareResidualBlock(nn.Module):
         stride: int = 1,
         downsample: Optional[nn.Sequential] = None,
     ):
+        super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.manifold = manifold
@@ -56,7 +120,8 @@ class PoincareResidualBlock(nn.Module):
         if self.downsample is not None:
             residual = self.downsample(residual)
 
-        x = self.manifold.mobius_add(x, residual) # TODO: add addition op to Manifold
+        # TODO: add addition op to Manifold
+        x = self.manifold.mobius_add(x, residual)
         x = self.relu(x)
 
         return x
@@ -69,6 +134,7 @@ class PoincareResNet(nn.Module):
         group_depths: list[int],
         manifold: PoincareBall,
     ):
+        super().__init__()
         self.channel_sizes = channel_sizes
         self.group_depths = group_depths
         self.manifold = manifold
@@ -103,10 +169,9 @@ class PoincareResNet(nn.Module):
         )
 
         self.avg_pool = hnn.HAvgPool2d(kernel_size=8, manifold=manifold)
-
         self.fc = hnn.HLinear(in_features=channel_sizes[2], out_features=10, manifold=manifold)
 
-    def foward(self, x: ManifoldTensor) -> ManifoldTensor:
+    def forward(self, x: ManifoldTensor) -> ManifoldTensor:
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
@@ -114,8 +179,7 @@ class PoincareResNet(nn.Module):
         x = self.group2(x)
         x = self.group3(x)
         x = self.avg_pool(x)
-        # This will fail, need to add squeeze
-        x = self.fc(x)
+        x = self.fc(x.squeeze())
         return x
 
     def _make_group(
@@ -156,3 +220,88 @@ class PoincareResNet(nn.Module):
             )
 
         return nn.Sequential(*layers)
+
+# Let's create a thin ResNet with a depth of 20 layers.
+net = PoincareResNet(
+    channel_sizes=[4, 8, 16],
+    group_depths=[3, 3, 3],
+    manifold=manifold,
+).cuda()
+
+
+########################################################################
+# 4. Define a Loss function and optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Let's use a Classification Cross-Entropy loss and RiemannianAdam optimizer.
+# Adam is preferred because hyperbolic linear layers can sometimes have training
+# difficulties early on due to poor initialization.
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+# net.parameters() includes the learnable curvature "c" of the manifold.
+from hll.optim import RiemannianAdam
+
+optimizer = RiemannianAdam(net.parameters(), lr=0.001)
+
+
+########################################################################
+# 5. Train the network
+# ^^^^^^^^^^^^^^^^^^^^
+# We simply have to loop over our data iterator, project the inputs onto the
+# manifold, and feed them to the network and optimize.
+
+from hll.tensors import TangentTensor
+
+for epoch in range(20):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].cuda(), data[1].cuda()
+
+        # move the inputs to the manifold
+        tangents = TangentTensor(data=inputs, man_dim=1, manifold=manifold)
+        manifold_inputs = manifold.expmap(tangents)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(manifold_inputs)
+        loss = criterion(outputs.tensor, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        print(f"[{epoch + 1}, {i + 1:5d}] loss: {loss.item():.3f}")
+        running_loss = 0.0
+
+print("Finished Training")
+
+
+########################################################################
+# 6. Test the network on the test data
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Let us look at how the network performs on the whole dataset.
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].cuda(), data[1].cuda()
+
+        # move the images to the manifold
+        tangents = TangentTensor(data=images, man_dim=1, manifold=manifold)
+        manifold_images = manifold.expmap(tangents)
+
+        # calculate outputs by running images through the network
+        outputs = net(manifold_images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.tensor, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the network on the 10000 test images: {100 * correct // total} %")
