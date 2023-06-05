f56164a200de259fac7880262eccf139b8123cb3
maxvanspengler <max.van.spengler@hotmail.com>
2023-06-05 14:44:07 +0200

    Added embedding to module imports

--- hll/nn/__init__.py
+++ hll/nn/__init__.py
@@ -3,6 +3,7 @@ from .modules.batchnorm import HBatchNorm, HBatchNorm2d
 from .modules.change_manifold import ChangeManifold
 from .modules.container import TangentSequential
 from .modules.convolution import HConvolution2d
+from .modules.embedding import HEmbedding
 from .modules.flatten import HFlatten
 from .modules.linear import HLinear
 from .modules.pooling import HMaxPool2d
