c187e95fdf71444cdea5defb2301f4e8258c3137
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2023-08-25 17:58:34 -0500

    Included both samplers.

--- tutorials/hyperbolic_vit_tutorial.py
+++ tutorials/hyperbolic_vit_tutorial.py
@@ -6,11 +6,13 @@ import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 seed = 0
 
-torch.use_deterministic_algorithms(True)
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 np.random.seed(seed)
 random.seed(seed)
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 # --------------------
 
@@ -27,6 +29,7 @@ from fastai.data.external import URLs, untar_data
 from torch.utils.data import DataLoader, Subset
 from torch.utils.data.sampler import Sampler
 from torchvision.datasets import ImageFolder
+import multiprocessing
 
 
 def get_labels_to_indices(labels: list) -> dict[str, np.ndarray]:
@@ -42,7 +45,7 @@ def get_labels_to_indices(labels: list) -> dict[str, np.ndarray]:
     return labels_to_indices
 
 
-class UniqueClassSampler(Sampler):
+class UniqueClassSampler1(Sampler):
     def __init__(self, labels, m_per_class, seed):
         self.length = len(labels) * m_per_class
         self.labels_to_indices = get_labels_to_indices(labels)
@@ -69,6 +72,33 @@ class UniqueClassSampler(Sampler):
         idx_list = np.stack(idx_list).reshape(len(self.labels), -1, self.m_per_class)
         idx_list = idx_list.transpose(1, 0, 2).reshape(-1).tolist()
         return iter(idx_list)
+    
+class UniqueClassSampler2(Sampler):
+    def __init__(self, labels, m_per_class, seed):
+        self.labels_to_indices = get_labels_to_indices(labels)
+        self.labels = sorted(list(self.labels_to_indices.keys()))
+        self.m_per_class = m_per_class
+        self.seed = seed
+        self.epoch = 0
+
+    def __len__(self):
+        return len(self.labels) * self.m_per_class
+    
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+    def __iter__(self):
+        idx_list = []
+        g = torch.Generator()
+        g.manual_seed(self.seed * 10000 + self.epoch)
+        idx = torch.randperm(len(self.labels), generator=g).tolist()
+        for i in idx:
+            t = self.labels_to_indices[self.labels[i]]
+            replace = len(t) < self.m_per_class
+            idx_list += np.random.choice(t, size=self.m_per_class, replace=replace).tolist()
+        return iter(idx_list)
+
+    
 
 
 # --------------------
@@ -89,8 +119,7 @@ imagenette_classes = [
 
 train_transform = transforms.Compose(
     [
-        transforms.Resize(256),
-        transforms.RandomResizedCrop(224),
+        transforms.RandomResizedCrop(224, interpolation=transforms.InterpolationMode("bicubic")),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
@@ -101,34 +130,36 @@ trainset.classes = imagenette_classes
 
 val_transform = transforms.Compose(
     [
-        transforms.Resize(256),
+        transforms.Resize(224),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ]
 )
 valset = ImageFolder(path / "val", transform=val_transform)
-subset = np.random.choice(len(valset), size=1000, replace=False)
-valset = Subset(valset, subset)
 valset.classes = imagenette_classes
 
-n_sampled_labels = 16
+n_sampled_labels = 450
 m_per_class = 2
 batch_size = n_sampled_labels * m_per_class
 
-sampler = UniqueClassSampler(labels=trainset.targets, m_per_class=m_per_class, seed=seed)
+sampler = UniqueClassSampler2(labels=trainset.targets, m_per_class=m_per_class, seed=seed)
 
 trainloader = DataLoader(
     dataset=trainset,
     sampler=sampler,
     batch_size=batch_size,
-    num_workers=8,
+    num_workers=multiprocessing.cpu_count(),
+    pin_memory=True,
+    drop_last=True,
 )
 
 valloader = DataLoader(
     dataset=valset,
     batch_size=batch_size,
-    num_workers=8,
+    num_workers=multiprocessing.cpu_count(),
+    pin_memory=True,
+    drop_last=False,
 )
 
 # --------------------
@@ -152,6 +183,7 @@ class HyperbolicVIT(nn.Module):
         super().__init__()
         self.vit = timm.create_model(vit_name, pretrained=pretrained)
         self.remove_head()
+        self.freeze_patch_embed()
         self.fc = nn.Linear(vit_dim, emb_dim)
         self.manifold = manifold
         self.clip_r = clip_r
@@ -176,6 +208,13 @@ class HyperbolicVIT(nn.Module):
         target = {"head", "fc", "head_dist", "head_drop"}
         for x in names & target:
             self.vit.add_module(x, nn.Identity())
+
+    def freeze_patch_embed(self):
+        def fr(m):
+            for param in m.parameters():
+                param.requires_grad = False
+        fr(self.vit.patch_embed)
+        fr(self.vit.pos_drop)
 
 
 hvit = HyperbolicVIT(
@@ -246,10 +285,11 @@ def eval_recall_k(k):
 
 # --------------------
 
-num_epochs = 2
+num_epochs = 100
 for epoch in range(num_epochs):
     sampler.set_epoch(epoch)
     running_loss = 0.0
+    num_samples = 0
     for d_i, data in enumerate(trainloader):
         # get the inputs; data is a list of [inputs, labels]
         inputs = data[0].to(device)
@@ -268,15 +308,13 @@ for epoch in range(num_epochs):
                     z_j = ManifoldTensor(z[:, j], manifold=manifold)
                     loss += criterion(z_i, z_j)
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(hvit.parameters(), 3)
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
-        if d_i % 10 == 9:  # print every 10 mini-batches
-            recall_k = eval_recall_k(k)
-            print(
-                f"[{epoch + 1}, {d_i + 1:5d}] loss: {loss.item()/10:.3f} recall@{k}: {recall_k:.3f}"
-            )
-            running_loss = 0.0
+        running_loss += loss.item() * bs
+        num_samples += bs
+    recall_k = eval_recall_k(k)
+    print(f"[Epoch {epoch + 1}] loss: {running_loss/num_samples:.3f} recall@{k}: {recall_k:.3f}")
 
 print("Finished Training")
